e08910b68c02685f52a13b4e1b57f5178237b70a
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-08-28 10:57:17 +1000

    Added extra functionality and header comment to etch_a_sketch.py

--- etch-a-sketch/etch_a_sketch.py
+++ etch-a-sketch/etch_a_sketch.py
@@ -1,3 +1,20 @@
+"""
+etch_a_sketch.py
+
+A program that lets your artistic creativity shine!
+You can control the cursor using WASD or the arrow keys
+Other controls are:
+    - c: clear
+    - q: quit
+    - t: teleport
+    - b: set colour to black
+    - l: set colour to blue
+    - r: set colour to red
+    - p: set colour to pink
+    - y: set colour to yellow
+
+Inspired from day 19 of 100 days of code
+"""
 from turtle import Turtle, Screen
 
 t = Turtle()
@@ -30,6 +47,10 @@ def clear():
     t.pendown()
 
 
+def quit_game():
+    screen.bye()
+
+
 def teleport():
     x = screen.numinput("Teleport", "x co-ordinate: ")
     y = screen.numinput("Teleport", "y co-ordinate: ")
@@ -37,6 +58,26 @@ def teleport():
     t.goto(x, y)
     t.pendown()
     screen.listen()
+
+
+def black():
+    t.color('black')
+
+
+def blue():
+    t.color('blue')
+
+
+def red():
+    t.color('red')
+
+
+def pink():
+    t.color('pink')
+
+
+def yellow():
+    t.color('yellow')
 
 
 screen.listen()
@@ -49,7 +90,12 @@ screen.onkey(key="Left", fun=turn_counter_clockwise)
 screen.onkey(key='d', fun=turn_clockwise)
 screen.onkey(key="Right", fun=turn_clockwise)
 screen.onkey(key='c', fun=clear)
+screen.onkey(key='q', fun=quit_game)
 screen.onkey(key='t', fun=teleport)
-
+screen.onkey(key='b', fun=black)
+screen.onkey(key='l', fun=blue)
+screen.onkey(key='r', fun=red)
+screen.onkey(key='p', fun=pink)
+screen.onkey(key='y', fun=yellow)
 
 screen.exitonclick()
