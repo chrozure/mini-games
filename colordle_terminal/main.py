15d5e0e9358f31e30650d49e0a1efb7d17b2cfa5
Harry Zhang <z5363493@ad.unsw.edu.au>
2025-04-16 11:08:11 +1000

    Implemented basic bot

--- colordle_terminal/main.py
+++ colordle_terminal/main.py
@@ -23,25 +23,30 @@ def main():
     correct_answer = tuple(possible_numbers)
 
     num_guesses = 0
+    bot_guesses = []
 
     game_still_running = True
     while game_still_running:
 
+        # get human guess
         guess = input(f"Guess a permutation of 1-{num_slots}: ").split()
         if len(guess) != num_slots:
             print(f"You need to guess {num_slots} numbers (separated by spaces)!")
             continue
 
+        # check duplicate numbers
         if not is_easy_mode and len(set(guess)) != num_slots:
             print("You cannot have duplicate guesses in normal mode")
             continue
 
         guess = tuple(map(int, guess))
 
+        # check numbers are in bound
         if not is_easy_mode and any(num < 1 or num > num_slots for num in guess):
             print("You cannot guess random numbers in normal mode")
             continue
 
+        # calculate score
         total_matching = 0
         for i in range(len(guess)):
             if guess[i] == correct_answer[i]:
@@ -53,7 +58,29 @@ def main():
             print("Congratulations! You win.")
             print(f"The correct answer was {correct_answer}")
             print(f"You won in {num_guesses} guesses.")
-            game_still_running = False
+            return
+
+        # construct bot guess
+        while True:
+            random.shuffle(possible_numbers)
+            bot_guess = tuple(possible_numbers)
+
+            if bot_guess not in bot_guesses:
+                break
+
+        bot_guesses.append(bot_guess)
+        bot_total_matching = 0
+        for i in range(len(bot_guess)):
+            if bot_guess[i] == correct_answer[i]:
+                bot_total_matching += 1
+
+        print(f"The bot guessed {bot_guess}")
+        print(f"The bot got {bot_total_matching} positions correct")
+        if bot_total_matching == num_slots:
+            print("The bot wins!")
+            print(f"The correct answer was {correct_answer}")
+            print(f"The bot took {len(bot_guesses)} guesses.")
+            return
 
 
 if __name__ == "__main__":
